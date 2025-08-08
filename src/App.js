0db18c7608b76749208ff013293805955308291c
NicocosFA <nicolas.fuentes.alfaro@gmail.com>
2025-08-08 11:36:03 -0400

    ComponenteCatalogo
    ComponenteCatalogo, CardJuego y CardBotones mostrandose en el inicio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from "./img/spaceinvaders.gif";
 import "./App.css";
 import Componenteheader from "./componentes/Componenteheader";
 import Footer from "./componentes/Footer";
+import Componentecatalogo from "./componentes/Componentecatalogo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Componenteheader />
       <main className="App">
         <header className="App-header">
+          <Componentecatalogo titulo="PlayStation 5" />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ src/componentes/Componentecatalogo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CardJuego from "./CardJuego";
+import CardBotones from "./CardBotones";
+import "../estilos/Componentecatalogo.css";
+
+export default function Componentecatalogo() {
+  return (
+    <div className="catalogo-container">
+      <h1 className="catalogo-title">PLAYSTATION 5</h1>
+
+      <div className="contenedor-principal">
+        <div className="columna-juegos">
+          <CardJuego />
+        </div>
+        <div className="columna-botones">
+          <CardBotones />
+        </div>
+      </div>
+    </div>
+  );
+}
